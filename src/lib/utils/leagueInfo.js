2a7d1cd9af83efd14bf6023ae1ee577dccb70c1b
bscrannell <bscrannell@gmail.com>
2025-08-14 22:49:03 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "your_league_id"; // your league ID
+export const leagueID = "1180168753995476992"; // your league ID
 export const leagueName = "Countdown County FFL"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
